3860ce3528e3680cea865e425b0b5ae2e272b746
Gardusio <fabiobarbieri.dev@gmail.com>
2024-03-18 17:32:15 +0000

    WIP: adds ASHResnet18 class with shape_activations hook definition

--- models/resnet.py
+++ models/resnet.py
@@ -11,6 +11,30 @@ class BaseResNet18(nn.Module):
     def forward(self, x):
         return self.resnet(x)
 
+# Modifies 'BaseResNet18' including the Activation Shaping Module
+class ASHResNet18(nn.Module):
+    def __init__(self):
+        super(ASHResNet18, self).__init__()
+        self.resnet = resnet18(weights=ResNet18_Weights)
+        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 7)
+
+    def forward(self, x):
+        return self.resnet(x)
+    
+    # WIP: custom hook, takes M as param so it can be used both for Point 2 and 3
+    def shape_activations(M):
+    # the hook signature
+      def hook(model, input, output):
+        # Binarize A and M
+        # return element-wise product A * M
+        print("activation shapes")
+      return hook
+
+
+
+
+
+
 ######################################################
 # TODO: either define the Activation Shaping Module as a nn.Module
 #class ActivationShapingModule(nn.Module):
