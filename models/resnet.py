1b0973f6666ae9bf06e8c41268cb0dca5221caee
Gardusio <fabiobarbieri.dev@gmail.com>
2024-04-10 13:05:10 +0000

    WIP: just binarizing A experiment (M = 1)

--- models/resnet.py
+++ models/resnet.py
@@ -23,36 +23,37 @@ class ASHResNet18(nn.Module):
 
     def random_shape_activations(self, model, input, output):
       
-      M = torch.randint(0, 2, output.shape).cuda()
-      total_elements = M.numel()
-      num_zeros = int(total_elements * 0.33)
-      num_ones = total_elements - num_zeros
+      #M = torch.randint(0, 2, output.shape).cuda()
+      #total_elements = M.numel()
+      #num_zeros = int(total_elements * 0)
+      #num_ones = total_elements - num_zeros
 
       # Reshape the tensor to a 1D tensor
-      random_tensor_flat = M.view(-1)
+      #random_tensor_flat = M.view(-1)
 
       # Set appropriate number of elements to 0 and 1
-      random_tensor_flat[:num_zeros] = 0
-      random_tensor_flat[num_zeros:] = 1
+      #random_tensor_flat[:num_zeros] = 0
+      #random_tensor_flat[num_zeros:] = 1
 
-      random_tensor_flat = random_tensor_flat[torch.randperm(total_elements)]
+      #random_tensor_flat = random_tensor_flat[torch.randperm(total_elements)]
 
       # Reshape the tensor back to its original shape
-      M_binary = random_tensor_flat.view(output.shape).float()
+      #M_binary = random_tensor_flat.view(output.shape).float()
  
-      # Binarize both A and M using threshold=0 for clarity
+      # Binarize both A and M
       A_binary = (output > 0).float().cuda()
 
       # Element-wise product for activation shaping
-      shaped_output = A_binary * M_binary
+      shaped_output = A_binary
       return shaped_output
 
 
 
     def register_hooks(self):
       resnet = self.resnet
-      h4 = resnet.layer4[1].conv2.register_forward_hook(self.random_shape_activations)
-      return [h4]
+      h4 = resnet.layer3.register_forward_hook(self.random_shape_activations)
+      
+      return [h3]
 
 
     def remove_hooks(self, hooks):
