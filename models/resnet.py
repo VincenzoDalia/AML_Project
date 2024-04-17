0c2abb57539b3a0c5f4faf3976f4d947d4ceb1b6
Gardusio <fabiobarbieri.dev@gmail.com>
2024-04-17 11:09:02 +0000

    Fixes shaping binarization

--- models/resnet.py
+++ models/resnet.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torchvision.models import resnet18, ResNet18_Weights
 
+
 class BaseResNet18(nn.Module):
     def __init__(self):
         super(BaseResNet18, self).__init__()
@@ -10,6 +11,7 @@ class BaseResNet18(nn.Module):
 
     def forward(self, x):
         return self.resnet(x)
+
 
 # Modifies 'BaseResNet18' including the Activation Shaping Module
 class ASHResNet18(nn.Module):
@@ -22,63 +24,51 @@ class ASHResNet18(nn.Module):
         return self.resnet(x)
 
     def random_shape_activations(self, model, input, output):
-      
-      #M = torch.randint(0, 2, output.shape).cuda()
-      #total_elements = M.numel()
-      #num_zeros = int(total_elements * 0)
-      #num_ones = total_elements - num_zeros
 
-      # Reshape the tensor to a 1D tensor
-      #random_tensor_flat = M.view(-1)
+        output_shape = output.shape
+        num_elements = output.numel()
+        num_ones = int(num_elements * 0.5)
+        # Create a binary tensor with the appropriate number of ones
+        random_indices = torch.randperm(num_elements)[:num_ones]
+        M_binary = torch.zeros(num_elements, device=output.device)
+        M_binary[random_indices] = 1
+        # Reshape the tensor to match the output shape
+        M_binary = M_binary.view(output_shape)
 
-      # Set appropriate number of elements to 0 and 1
-      #random_tensor_flat[:num_zeros] = 0
-      #random_tensor_flat[num_zeros:] = 1
+        A_binary = (output > 0).float()
 
-      #random_tensor_flat = random_tensor_flat[torch.randperm(total_elements)]
+        # Element-wise product for activation shaping
+        shaped_activation = A_binary * M_binary
 
-      # Reshape the tensor back to its original shape
-      #M_binary = random_tensor_flat.view(output.shape).float()
- 
-      # Binarize both A and M
-      A_binary = (output > 0).float().cuda()
-
-      # Element-wise product for activation shaping
-      shaped_output = A_binary
-      return shaped_output
-
-
+        return shaped_activation
 
     def register_hooks(self):
-      resnet = self.resnet
-      h4 = resnet.layer3.register_forward_hook(self.random_shape_activations)
-      
-      return [h3]
-
+        resnet = self.resnet
+        h3 = resnet.layer3[1].conv2.register_forward_hook(self.random_shape_activations)
+        h4 = resnet.layer3[0].conv2.register_forward_hook(self.random_shape_activations)
+        return [h3, h4]
 
     def remove_hooks(self, hooks):
-      for h in hooks:
-        h.remove()
-
-
+        for h in hooks:
+            h.remove()
 
 
 ######################################################
 # TODO: either define the Activation Shaping Module as a nn.Module
-#class ActivationShapingModule(nn.Module):
-#...
+# class ActivationShapingModule(nn.Module):
+# ...
 #
 # OR as a function that shall be hooked via 'register_forward_hook'
-#def activation_shaping_hook(module, input, output):
-#...
+# def activation_shaping_hook(module, input, output):
+# ...
 #
 ######################################################
 # TODO: modify 'BaseResNet18' including the Activation Shaping Module
-#class ASHResNet18(nn.Module):
+# class ASHResNet18(nn.Module):
 #    def __init__(self):
 #        super(ASHResNet18, self).__init__()
 #        ...
-#    
+#
 #    def forward(self, x):
 #        ...
 #
