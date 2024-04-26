c351433de8115d07b478aaaef0507ef18b3a6bc1
Gardusio <fabiobarbieri.dev@gmail.com>
2024-04-26 10:21:28 +0200

    refactoring

--- models/resnet.py
+++ models/resnet.py
@@ -28,39 +28,43 @@ class ASHResNet18(nn.Module):
     def shape_activation(self, layer_activation, M):
         # Binarize both A and M
         A_binary = (layer_activation > 0).float()
-        
+
         # consider removing M binarization here
         # random_shaping_hook can binarize instead for point 2
         # ext 2 requires retaining M as it is
         M_binary = (M > 0).float()
 
         # Element-wise product for activation shaping
-        return A_binary * M
-
+        return A_binary * M_binary
 
     def random_shape_activations(self, mask_ratio):
         def hook(model, input, output):
-          num_elements = output.numel()
-          num_ones = int(num_elements * mask_ratio)
-          # Create a binary tensor with the appropriate number of ones
-          random_indices = torch.randperm(num_elements)[:num_ones]
-          M_binary = torch.zeros(num_elements, device=output.device)
-          M_binary[random_indices] = 1
-          # Reshape the tensor to match the output shape
-          M_binary = M_binary.view(output.shape)
-          
-          return self.shape_activation(output, M_binary)
-        
+            num_elements = output.numel()
+            num_ones = int(num_elements * mask_ratio)
+            # Create a binary tensor with the appropriate number of ones
+            random_indices = torch.randperm(num_elements)[:num_ones]
+            M_binary = torch.zeros(num_elements, device=output.device)
+            M_binary[random_indices] = 1
+
+            # Reshape the tensor to match the output shape
+            M_binary = M_binary.view(output.shape)
+
+            return self.shape_activation(output, M_binary)
+
         return hook
+
+    # TODO: domain_adapt_activations():
 
     def register_random_shaping_hooks(self, mask_ratio):
         random_maps_hook = self.random_shape_activations(mask_ratio)
-        
+
         h2 = self.resnet.layer2[1].conv2.register_forward_hook(random_maps_hook)
-        #h3 = self.resnet.layer3[0].conv2.register_forward_hook(random_maps_hook)
-        
-        #self.hooks.append(h3)
+        # h3 = self.resnet.layer3[0].conv2.register_forward_hook(random_maps_hook)
+
+        # self.hooks.append(h3)
         self.hooks.append(h2)
+
+    # TODO: register_domain_adapt_hooks()
 
     def remove_hooks(self):
         for h in self.hooks:
