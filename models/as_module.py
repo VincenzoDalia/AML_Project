189508b16954b95f8bc42aa643086e1b728e255f
Gardusio <fabiobarbieri.dev@gmail.com>
2024-06-02 12:52:58 +0000

    cleanups

--- models/as_module.py
+++ models/as_module.py
@@ -13,29 +13,39 @@ class ActivationShapingModule:
         result[m > 0] = 1.0
         return result
 
-    def topK_shape(self, A, M, t):
-        M_binary = self.binarize_matrix(M)
-
-        k = int(self.topK_treshold * A.numel())
-
+    def get_topK(self, A, k):
         _, top_indices = torch.topk(A.flatten(), k)
         # Creating the new A tensor with top K values
         A_topK = torch.zeros_like(A)
         A_topK.view(-1)[top_indices] = A.view(-1)[top_indices]
 
-        return A_topK * M_binary
+        return A_topK
+
+    def topK_shape_or(self, A, M, t):
+        if self.binarize:
+          M = self.binarize_matrix(M)
+
+        if self.topK_treshold == 1:
+          return A * M 
+
+        k = int(self.topK_treshold * A.numel())
+        A_topK = self.get_topK(A, k)
+        
+        return A_topK * M
+
+
 
     def binarize_then_shape(self, A, M):
         M_binary = self.binarize_matrix(M)
         A_binary = self.binarize_matrix(A)
         return A_binary * M_binary
 
-    def shape_activation(self, activation_map, M):
+    def shape_activation(self, A, M):
         """
         Shape the output of a layer performing A * M
 
         Parameters:
-        activation_map : The output activation map of a layer.
+        A : The output activation map of a layer.
 
         M: the matrix with which to perform the shaping.
 
@@ -43,9 +53,9 @@ class ActivationShapingModule:
         The result of A * M
         """
         if self.topK:
-            return self.topK_shape(activation_map, M, self.topK_treshold)
+            return self.topK_shape(A, M, self.topK_treshold)
 
         if self.binarize:
-            return self.binarize_then_shape(activation_map, M)
+            return self.binarize_then_shape(A, M)
 
-        return activation_map * M
+        return A * M
